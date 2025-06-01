96e6be24f9240fb488070779a3ce43551c7425df
pra-sat <flybirdzone@gmail.com>
2025-06-01 14:10:37 +0700

    Update script.js

--- admin/script.js
+++ admin/script.js
@@ -1,6 +1,37 @@
 const SHEET_API = 'https://script.google.com/macros/s/AKfycbxdxUvmwLS3_nETwGLk4J8ipPq2LYNSWyhJ2ZwVsEJQgONG11NSSX3jVaeqWCU1TXvE5g/exec';
 const liffId = '2007421084-2OgzWbpV';
 
+document.addEventListener('DOMContentLoaded', async () => {
+  const loading = document.getElementById('loadingOverlay');
+
+  try {
+    await liff.init({ liffId });
+
+    if (!liff.isLoggedIn()) {
+      liff.login();
+      return;
+    }
+
+    const profile = await liff.getProfile();
+    const userId = profile.userId;
+
+    const res = await fetch(`${SHEET_API}?action=check_admin&userId=${userId}`);
+    const result = await res.json();
+
+    if (result.isAdmin) {
+      // ✅ ถ้าเป็นแอดมิน → ไปหน้า main_admin.html
+      window.location.href = 'main_admin.html';
+    } else {
+      // ❌ ไม่ใช่แอดมิน → แสดงหน้า user
+      document.getElementById('userView').classList.remove('hidden');
+    }
+  } catch (err) {
+    await liff.closeWindow();
+  } finally {
+    loading.classList.add('hidden');
+  }
+});
+
 // ✅ เพิ่ม: เก็บข้อมูลผู้ใช้ LINE
 const profile = await liff.getProfile();
 const userId = profile.userId;
@@ -47,38 +78,3 @@ document.getElementById('submitFeedbackBtn').addEventListener('click', async () 
     alert("❌ เกิดข้อผิดพลาด: " + result.message);
   }
 });
-
-
-
-
-document.addEventListener('DOMContentLoaded', async () => {
-  const loading = document.getElementById('loadingOverlay');
-
-  try {
-    await liff.init({ liffId });
-
-    if (!liff.isLoggedIn()) {
-      liff.login();
-      return;
-    }
-
-    const profile = await liff.getProfile();
-    const userId = profile.userId;
-
-    const res = await fetch(`${SHEET_API}?action=check_admin&userId=${userId}`);
-    const result = await res.json();
-
-    if (result.isAdmin) {
-      // ✅ ถ้าเป็นแอดมิน → ไปหน้า main_admin.html
-      window.location.href = 'main_admin.html';
-    } else {
-      // ❌ ไม่ใช่แอดมิน → แสดงหน้า user
-      document.getElementById('userView').classList.remove('hidden');
-    }
-  } catch (err) {
-    await liff.closeWindow();
-  } finally {
-    loading.classList.add('hidden');
-  }
-});
-

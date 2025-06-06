b00eb2ce52243ea961cd9b4493bf592a311f57ca
pra-sat <flybirdzone@gmail.com>
2025-06-06 23:48:14 +0700

    Update script.js

--- info_ad/main_admin/scan/script.js
+++ info_ad/main_admin/scan/script.js
@@ -92,7 +92,8 @@ class QRScanner {
       price,
       point,
       note,
-      timestamp: new Date().toISOString(),
+      //timestamp: new Date().toISOString(),
+      timestamp: scanner.getThaiDateTime(), // รูปแบบ dd/MM/yyyy, HH:mm:ss
       admin: this.adminName
     };
 
@@ -144,6 +145,17 @@ class QRScanner {
         (decodedText) => this.onScanSuccess(decodedText)
       );
     });
+  }
+
+  getThaiDateTime(dateObj = new Date()) {
+    const d = dateObj;
+    const day = d.getDate().toString().padStart(2, '0');
+    const month = (d.getMonth() + 1).toString().padStart(2, '0');
+    const year = d.getFullYear();
+    const hour = d.getHours().toString().padStart(2, '0');
+    const minute = d.getMinutes().toString().padStart(2, '0');
+    const second = d.getSeconds().toString().padStart(2, '0');
+    return `${day}/${month}/${year}, ${hour}:${minute}:${second}`;
   }
 
   async manualSearch() {

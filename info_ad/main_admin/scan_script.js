e41e4679c26547e4fdbe299c48e0f123a4fda61d
pra-sat <flybirdzone@gmail.com>
2025-06-07 07:48:18 +0700

    Update scan_script.js

--- info_ad/main_admin/scan_script.js
+++ info_ad/main_admin/scan_script.js
@@ -1,4 +1,4 @@
-// script.js (ES6 Class)
+// scan_script.js
 const GAS_ENDPOINTS = 'https://script.google.com/macros/s/AKfycbxdxUvmwLS3_nETwGLk4J8ipPq2LYNSWyhJ2ZwVsEJQgONG11NSSX3jVaeqWCU1TXvE5g/exec';
 const liffIdss = '2007421084-2OgzWbpV';
 
